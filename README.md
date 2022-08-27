fd943d2b8f2b7bc8b3210fece8355e6fa37b9a60
baba-s <shotababa1206@gmail.com>
2022-08-27 10:11:47 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Kogane.Disposable
+# Kogane Disposable
+
+UniRx の Disposable クラスのみを抽出したパッケージ
+
+## 備考
+
+* 本パッケージは以下の MIT License のリポジトリの機能を使用しております
+    * https://github.com/neuecc/UniRx
---
+++ Runtime/Disposable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Kogane
+{
+    public static class Disposable
+    {
+        public static IDisposable Empty => EmptyDisposable.Singleton;
+
+        public static IDisposable Create( Action disposeAction )
+        {
+            return new AnonymousDisposable( disposeAction );
+        }
+
+        private sealed class EmptyDisposable : IDisposable
+        {
+            public static EmptyDisposable Singleton { get; } = new();
+
+            private EmptyDisposable()
+            {
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+
+        private sealed class AnonymousDisposable : IDisposable
+        {
+            private readonly Action m_dispose;
+
+            private bool m_isDisposed;
+
+            public AnonymousDisposable( Action dispose )
+            {
+                m_dispose = dispose;
+            }
+
+            public void Dispose()
+            {
+                if ( m_isDisposed ) return;
+
+                m_dispose();
+                m_isDisposed = true;
+            }
+        }
+    }
+}
